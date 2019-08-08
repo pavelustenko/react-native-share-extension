70fc6b5e7ed7f0a6ccd84d272d6ca65987b4fa52
pavelustenko <48822891+pavelustenko@users.noreply.github.com>
2019-08-08 16:13:54 +0300

    Update ShareModule.java

--- android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
+++ android/src/main/java/com/alinz/parkerdan/shareextension/ShareModule.java
@@ -8,69 +8,101 @@ import com.facebook.react.bridge.WritableMap;
 import com.facebook.react.bridge.Arguments;
 
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 
 import android.graphics.Bitmap;
+import android.os.Environment;
+import android.webkit.MimeTypeMap;
+
 import java.io.InputStream;
 
 
 public class ShareModule extends ReactContextBaseJavaModule {
 
 
-  public ShareModule(ReactApplicationContext reactContext) {
-      super(reactContext);
-  }
+    public ShareModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+    }
 
-  @Override
-  public String getName() {
-      return "ReactNativeShareExtension";
-  }
+    @Override
+    public String getName() {
+        return "ReactNativeShareExtension";
+    }
 
-  @ReactMethod
-  public void close() {
-    getCurrentActivity().finish();
-  }
+    @ReactMethod
+    public void close() {
+        getCurrentActivity().finish();
+    }
 
-  @ReactMethod
-  public void data(Promise promise) {
-      promise.resolve(processIntent());
-  }
+    @ReactMethod
+    public void data(Promise promise) {
+        promise.resolve(processIntent());
+    }
 
-  public WritableMap processIntent() {
-      WritableMap map = Arguments.createMap();
+    public WritableMap processIntent() {
+        WritableMap map = Arguments.createMap();
 
-      String value = "";
-      String type = "";
-      String action = "";
+        String value = "";
+        String type = "";
+        String action = "";
 
-      Activity currentActivity = getCurrentActivity();
+        Activity currentActivity = getCurrentActivity();
 
-      if (currentActivity != null) {
-        Intent intent = currentActivity.getIntent();
-        action = intent.getAction();
-        type = intent.getType();
-        if (type == null) {
-          type = "";
+        if (currentActivity != null) {
+            String filepath = Environment.getExternalStorageDirectory().getPath();
+            System.out.println(filepath);
+            Intent intent = currentActivity.getIntent();
+            action = intent.getAction();
+            type = intent.getType();
+            if (type == null) {
+                type = "";
+            }
+            if (Intent.ACTION_SEND.equals(action) && "text/plain".equals(type)) {
+                value = intent.getStringExtra(Intent.EXTRA_TEXT);
+            } else if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type))) {
+                Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if ("image/*".equals(type)) {
+                    String specifiedType = getMimeType(uri);
+                    if (specifiedType != null && !specifiedType.isEmpty()) {
+                        type = specifiedType;
+                    }
+                }
+                value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
+
+            } else if (Intent.ACTION_SEND.equals(action)) {
+                Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
+                if (type == "") {
+                    String specifiedType = getMimeType(uri);
+                    if(specifiedType!=null && !specifiedType.isEmpty()){
+                        type=specifiedType;
+                    }
+                }
+                value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
+            }
+        } else {
+            value = "";
+            type = "";
         }
-        if (Intent.ACTION_SEND.equals(action) && "text/plain".equals(type)) {
-          value = intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        map.putString("type", type);
+        map.putString("value", value);
+
+        return map;
+    }
+
+    public String getMimeType(Uri uri) {
+        String mimeType = null;
+        if (uri.getScheme().equals(ContentResolver.SCHEME_CONTENT)) {
+            ContentResolver cr = getReactApplicationContext().getContentResolver();
+            mimeType = cr.getType(uri);
+        } else {
+            String fileExtension = MimeTypeMap.getFileExtensionFromUrl(uri
+                    .toString());
+            mimeType = MimeTypeMap.getSingleton().getMimeTypeFromExtension(
+                    fileExtension.toLowerCase());
         }
-        else if (Intent.ACTION_SEND.equals(action) && ("image/*".equals(type) || "image/jpeg".equals(type) || "image/png".equals(type) || "image/jpg".equals(type) ) ) {
-          Uri uri = (Uri) intent.getParcelableExtra(Intent.EXTRA_STREAM);
-         value = "file://" + RealPathUtil.getRealPathFromURI(currentActivity, uri);
-
-       } else {
-         value = "";
-       }
-      } else {
-        value = "";
-        type = "";
-      }
-
-      map.putString("type", type);
-      map.putString("value",value);
-
-      return map;
-  }
+        return mimeType;
+    }
 }
